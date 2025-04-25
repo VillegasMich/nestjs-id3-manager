fa44201008f0ae9087d935800444b7eb9a728dfe
VillegasMich <manuel.villegas.michel@gmail.com>
2025-04-25 10:14:48 -0500

    feat: user model, service and controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,12 +4,14 @@ import { AppService } from './app.service';
 import { DatabaseModule } from './database/database.module';
 import { ConfigModule } from '@nestjs/config';
 import { MetadataModule } from './metadata/metadata.module';
+import { UserModule } from './user/user.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
     DatabaseModule,
     MetadataModule,
+    UserModule,
   ],
   controllers: [AppController],
   providers: [AppService],
---
+++ src/user/dto/update-user.dto.ts
@@ -0,0 +1,17 @@
+import { IsEmail, IsOptional, MinLength } from 'class-validator';
+
+export class UpdateUserDto {
+  @IsEmail()
+  @IsOptional()
+  email?: string;
+
+  @IsOptional()
+  @MinLength(6)
+  password?: string;
+
+  @IsOptional()
+  name?: string;
+
+  @IsOptional()
+  isActive?: boolean;
+}
